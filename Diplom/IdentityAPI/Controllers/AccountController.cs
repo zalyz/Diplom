3e86e1dc1ac245d98c1e72835699cf8cf9ad2abb
Viachaslau Draneu <Viachaslau_Draneu@epam.com>
2022-04-03 19:05:06 +0300

    Start commit

---
+++ Diplom/IdentityAPI/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using IdentityAPI.RequestModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<IdentityError>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromForm] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var language = GetUserLanguage(model.Language);
+            var user = new User
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                TimeZone = model.TimeZone,
+                Language = language,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+                return Ok(user.Id);
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
