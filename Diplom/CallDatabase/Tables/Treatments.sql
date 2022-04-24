bfcf845d097ab415e6816ff44d6f6092d3c44bc7
Viachaslau Draneu <Viachaslau_Draneu@epam.com>
2022-04-24 22:10:33 +0300

    Updated

--- Diplom/CallDatabase/Tables/Treatments.sql
+++ Diplom/CallDatabase/Tables/Treatments.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Treatments]
 (
 	[Id] INT NOT NULL PRIMARY KEY Identity(1,1), 
-    [MedicamentId] INT NOT NULL, 
     [CallId] INT NOT NULL,
-    Constraint [FK_Medicaments] Foreign key (MedicamentId) references [dbo].[Medicaments] (Id),
+    Constraint [FK_Calls] Foreign key (CallId) references [dbo].[Call] (Id),
 )
